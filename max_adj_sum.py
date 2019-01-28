25cce097c35578a38cc5023108812bf767e5c2ed
vitcmaestro <40243064+vitcmaestro@users.noreply.github.com>
2019-01-28 16:10:03 +0530

    Update max_adj_sum.py

--- max_adj_sum.py
+++ max_adj_sum.py
@@ -6,4 +6,9 @@ for i in range(n):
     for j in range(i+1,n):
         add = add+ a[j]
     res.append(add)
+for i in range(n):
+    add = 0
+    for j in range(i+1):
+        add = add+ a[j]
+    res.append(add)
 print(max(res))
