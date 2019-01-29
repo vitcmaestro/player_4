9bb49ee55883ac1be81fcb351f90ffbc2f1f7ab0
vitcmaestro <40243064+vitcmaestro@users.noreply.github.com>
2019-01-29 16:02:58 +0530

    Update date_format.py

--- date_format.py
+++ date_format.py
@@ -1,12 +1,12 @@
 d,m,y = map(int,input().split('/'))
 res = True
-if(d<=0 or d>=31):
+if(d<=0 or d>31):
     res = False
-if(m<1 or m>=12):
+if(m<1 or m>12):
     res = False
 if(y > 9999):
     res = False
 if(res == True):
-    print("YES")
+    print("yes")
 else:
-    print("NO")
+    print("no")
