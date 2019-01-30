06bf1c4950dd859020a09842e63fdd7d8ce30f93
vitcmaestro <40243064+vitcmaestro@users.noreply.github.com>
2019-01-30 16:28:53 +0530

    Create if_num_lessthan_k_times.py

---
+++ if_num_lessthan_k_times.py
@@ -0,0 +1,16 @@
+n,k = map(int,input("").split())
+lis = list(map(int,input().split()))
+lis.sort()
+freq = [1]*n
+for i in range(n-2):
+    if(lis[i] == lis[i+1]):
+        lis.remove(lis[i+1])
+        freq[i]+=1
+c = 0
+for i in range(len(lis)):
+    if(freq[i]<=k):
+        if(c == 0):
+            print(lis[i],end ="")
+            c+=1
+        else:
+            print("",lis[i],end = "")
