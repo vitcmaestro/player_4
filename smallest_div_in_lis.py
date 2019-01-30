1ea8464b7646481cf38456fdfbfe5d52406914f7
vitcmaestro <40243064+vitcmaestro@users.noreply.github.com>
2019-01-30 16:10:04 +0530

    Update smallest_div_in_lis.py

--- smallest_div_in_lis.py
+++ smallest_div_in_lis.py
@@ -1,11 +1,13 @@
 n = int(input(""))
 lis = list(map(int,input().split()))
 res=1
+a=[]
 for i in range(len(lis)):
     c = 0
     for j in range(len(lis)):
         if(lis[j]%lis[i] == 0):
             c+=1
     if(c == len(lis)):
-        print(lis[i])
+        a.append(lis[i])
+print(min(a))
     
