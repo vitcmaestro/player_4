e532df94a1675dcc0df5faef0c4f7515003e51d2
vitcmaestro <40243064+vitcmaestro@users.noreply.github.com>
2019-01-30 21:19:06 +0530

    Update even_prefix_sumarr.py

--- even_prefix_sumarr.py
+++ even_prefix_sumarr.py
@@ -1,12 +1,14 @@
 n = int(input(""))
 a = list(map(int,input().split()))
-res = []
-for i in range(n):
-    ans = 0
-    if(a[i]%2 !=0):
-        res.append(str(a[i]))
+ans = a[0]
+print(a[0],end = "")
+for i in range(1,n):
+    if(a[i]%2 !=0 and i%2 != 0):
+        ans += a[i]
+        a[i] = ans
+    elif(a[i]%2 == 0):
+        ans += a[i]
+        a[i] = ans
     else:
-        for j in range(i+1):
-            ans += a[j]
-        res.append(str(ans))
-print(" ".join(res).strip())
+        ans+=a[i]
+    print("",a[i],end="")
