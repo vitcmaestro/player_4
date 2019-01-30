7b3790a1a9b86971eef311762d310a8d41d84c24
vitcmaestro <40243064+vitcmaestro@users.noreply.github.com>
2019-01-30 17:02:24 +0530

    Update prod_prediction.py

--- prod_prediction.py
+++ prod_prediction.py
@@ -2,11 +2,14 @@ n = int(input(""))
 lis = list(map(int,input().split()))
 c = 0
 prod = 1
+ten = 0
 for i in lis:
     if(i%2 != 0):
         c+=1
         prod = prod * i
-if(prod%2 == 0 and c>0):
+    if(i%10 == 0):
+        ten +=1
+if((prod%2 == 0 or ten>0)  and c>0):
     print("even")
 elif(c == 0):
     print("even")
