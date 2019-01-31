f8392abaa54998977ddf95ca62254f4de93b2045
vitcmaestro <40243064+vitcmaestro@users.noreply.github.com>
2019-01-31 16:16:28 +0530

    Create e_O_alt.py

---
+++ e_O_alt.py
@@ -0,0 +1,25 @@
+n = input("")
+c=0
+i=0
+miner = 0
+if(int(n[0])%2 == 0):
+    while(i<len(n)):
+        if(i%2 == 0 and int(n[i])%2 == 0):
+            c+=1
+        elif(i%2 != 0  and int(n[i])%2 !=0):
+            c+=1
+        if(c>miner):
+            miner = c
+        i+=1
+else:
+    while(i<len(n)):
+        if(i%2 != 0 and int(n[i])%2 == 0):
+            c+=1
+        elif(i%2 == 0  and int(n[i])%2 !=0):
+            c+=1
+        if(c>miner):
+            miner = c
+        
+            
+        i+=1
+print(miner)
