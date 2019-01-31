e44f146dc84fe5626ffc9910fea733ae4d9849ab
vitcmaestro <40243064+vitcmaestro@users.noreply.github.com>
2019-01-31 23:16:09 +0530

    Create is_k_consecutive_same.py

---
+++ is_k_consecutive_same.py
@@ -0,0 +1,17 @@
+n,k = map(int,input().split())
+a = []
+c = 0
+for i in range(n):
+    s = input("")
+    a.append(s)
+for i in range(n-1):
+    if(a[i] == a[i+1]):
+        c+=1
+    else:
+        if(c == k):
+            break
+        c = 0
+if(c == k):
+    print("yes")
+else:
+    print("no")
