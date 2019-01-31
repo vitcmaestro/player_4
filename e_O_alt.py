e17a685efd61eb88ff9b6e8be3bb1a75c641bf13
vitcmaestro <40243064+vitcmaestro@users.noreply.github.com>
2019-01-31 16:22:52 +0530

    Update e_O_alt.py

--- e_O_alt.py
+++ e_O_alt.py
@@ -2,24 +2,15 @@ n = input("")
 c=0
 i=0
 miner = 0
-if(int(n[0])%2 == 0):
-    while(i<len(n)-1):
-        if(i%2 == 0 and int(n[i])%2 == 0 and int(n[i+1])%2 != 0):
-            c+=1
-        elif(i%2 != 0  and int(n[i])%2 !=0 and int(n[i+1])%2 == 0):
-            c+=1
-        if(c>miner):
-            miner = c
-        i+=1
-else:
-    while(i<len(n)-1):
-        if(i%2 != 0 and int(n[i])%2 == 0 and int(n[i+1])%2 != 0):
-            c+=1
-        elif(i%2 == 0  and int(n[i])%2 !=0 and int(n[i+1])%2 == 0):
-            c+=1
-        if(c>miner):
-            miner = c
-        
-            
-        i+=1
+while(i<len(n)-1):
+    if(i%2 == 0 and int(n[i])%2 == 0 and int(n[i+1])%2 != 0):
+        c+=1
+    elif(i%2 != 0  and int(n[i])%2 !=0 and int(n[i+1])%2 == 0):
+        c+=1
+    if(c>miner):
+        miner = c
+    if((int(n[i])%2 != 0 and int(n[i+1])%2 != 0) or (int(n[i])%2 ==0 and int(n[i+1])%2 == 0)):
+        c = 0
+    i+=1
+
 print(miner+1)
