e7f493e0ff7f301895a1187bdc1fcba2543927d0
vitcmaestro <40243064+vitcmaestro@users.noreply.github.com>
2019-03-16 10:18:29 +0530

    Update Roman_army.py

--- Roman_army.py
+++ Roman_army.py
@@ -1,7 +1,7 @@
 n = int(input(""))
 a = list(map(int,input().split()))
 c = 0
-for k in range(2,n):
+for k in range(2,len(a)):
     for j in range(1,k):
         for i in range(j):
             if(a[i]>a[j]>a[k]):
